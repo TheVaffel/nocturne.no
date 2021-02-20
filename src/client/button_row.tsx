8ddbc0fcc447a929b28ea1c85ca0eebd4f09f787
Håkon Flatval <hkon20@hotmail.com>
2021-02-20 20:45:11 +0100

    Make tab buttons more fancy

--- src/client/button_row.tsx
+++ src/client/button_row.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 
+
+const tabTitleStyle = { backgroundColor: "powderblue", 
+                        display: "inline",
+                        padding: "5px",
+                        margin: "5px"}
+const TabTitle: React.FunctionComponent<React.PropsWithChildren<{}>> = (props) => (<div style={tabTitleStyle}>{props.children}</div>)
+
 export const TopButtonRow = () => (<div>
-       <Link to="/programming_tutorial">Tutorial Blog</Link>
-       <Link to="/dev_blog">Tech Blog</Link>
-       <Link to="/dev_blog">Rant Blog</Link>
-       <Link to="/programming_tutorial">Abandon All Hope Ye Who Click Here</Link>
-       <a href="/lol">Hmmmm</a>
+       <Link to="/"><TabTitle>Home</TabTitle></Link>
+       <Link to="/programming_tutorial"><TabTitle>Tutorial Blog</TabTitle></Link>
+       <Link to="/dev_blog"><TabTitle>Tech Blog</TabTitle></Link>
+       <Link to="/dev_blog"><TabTitle>Rant Blog</TabTitle></Link>
        </div>);
