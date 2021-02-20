04784c29ff1462270d9628c266da56141533bce9
Håkon Flatval <hkon20@hotmail.com>
2021-02-20 20:43:54 +0100

    Update root element id

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Hello } from './hello.tsx';
 import { Root } from './root.tsx';
 
 ReactDOM.render(
 	(<Root />),
-	document.getElementById('hello')
+	document.getElementById('root')
 );
