4b7754ba6dfc18db01f421d6565dc312836327ad
Håkon Flatval <hkon20@hotmail.com>
2021-02-28 16:08:23 +0100

    Remove comment of unused commit

--- src/client/content_wrapper.tsx
+++ src/client/content_wrapper.tsx
@@ -4,8 +4,7 @@ import { Suspense } from 'react';
 import { Route, Switch, Link, } from 'react-router-dom';
 import { DevBlogWrapper, devblogPath } from './dynamic_components/devblog/devblog_wrapper.tsx'
 
-import { // SettingUpABlog0,
-         SiteIndex } from './dynamic_components/dynamics.tsx';
+import { SiteIndex } from './dynamic_components/dynamics.tsx';
 
 
 export const ContentWrapper = () => (<div>
