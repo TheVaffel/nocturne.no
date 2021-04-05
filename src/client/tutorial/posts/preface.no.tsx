2594c54908127f5748b0c5be8fe1eadba96c93f0
Håkon Flatval <hkon20@hotmail.com>
2021-04-05 21:33:35 +0200

    Finish up Norwegian preface

--- src/client/tutorial/posts/preface.no.tsx
+++ src/client/tutorial/posts/preface.no.tsx
@@ -16,23 +16,7 @@ const PrefaceNo = (props: TutorialPostProps) => (
     men det er forstatt mange ubesvarte spørsmål. Er du heldig, kan du ett og annet om hvordan f. eks. en tradisjonell bil omdanner
     bensin til framdriftskraft, kanskje til og med har oversikt over de fysiske lovene som blir utnyttet i prosessen.
     <Db />
-    Men når <i>forstår</i> man hvordan en bil fungerer? Selv med ekspertkunnskap om fysiske lover, motoranatomi
-    og bilkjøring, vil det være spørsmål som står ubesvart, og som ligger et hakk utenfor hva vitenskapen tar for seg.
-    <Db />
-    <ul>
-    <li>"Hvorfor er de fysiske lovene som de er?"</li> 
-    <li>"Med hvilke incentiver ble (denne spesifikke) bilen konstruert?" </li>
-    <li>"Hvordan har mennesket fått evnen til å forestille seg og lage slike doninger?" </li>
-    </ul>
-    <Db />
-    Selvfølgelig vil de fleste si at det er unødvendig å kunne svare på disse spørsmålene for å <i>forstå</i> hvordan en bil fungerer.
-    Men om man har svarene på ett eller flere av disse spørsmålene, har man også dykket lenger inn i sammenhengen bilen
-    kommer fra og hører hjemme til. På samme måte vil kunnskap om de relevante fysiske formuleringene gi
-    en <i>dypere forståelse</i> for hvordan bilen hører hjemme i den fysiske virkeligheten vi lever i.
-    <Db />
-    Men når <i>forstår</i> man hvordan en bil fungerer? Tydeligvis kan man bruke evigheter på å grave seg ned i kunnskap for å få en
-    dypere <i>forståelse</i> for biler generelt, eller én bil spesielt. Det gir derfor mer mening å
-    eksplisitt sette en grense for hvor mye - og hva slags - kunnskap man bør inneha for å ha <i>forstått</i> en bil.
+    Men når <i>forstår</i> man hvordan en bil fungerer? 
     <Db />
     Hvor går grensen? 
     <Db />
@@ -59,22 +43,40 @@ const PrefaceNo = (props: TutorialPostProps) => (
     et uendelig jag etter sammenhenger og kunnskap, men for enhver betydning av ordet finnes det et kunnskapsnivå som er <i>godt nok</i>.
     <Db />
     <Db />
-    La oss snakke om programmering.
+    Uansett, la oss snakke om programmering.
     <Db />
     Målet mitt med denne bloggserien er å gi deg, min kjære leser, en innføring i programmering i Python. 
     Jeg vil gi deg en forståelse for hvordan programmering fungerer, og hva det kan brukes til.
 
     <Db />
-    Ordet <i>forståelse</i> i setningen over har jeg allerede brukt altfor mye plass på å definere, men det
-    betyr altså at jeg vil gi deg en innføring som er <i>god nok</i> til å gjøre en del interessante ting
+    Ordet <i>forståelse</i> i setningen over har jeg allerede brukt altfor mye plass på å definere.
+    I likhet med prosessen det er å <i>forstå</i> en bil, er det å <i>forstå</i> programmering et evighetsprosjekt,
+    og du vil sannsynligvis ha mange spørsmål igjen etter å ha lest gjennom det jeg har å si.
+    
+    Målet er derfor å gi deg en innføring som er <i>god nok</i> til å gjøre en del interessante ting
     med programmering <i>og</i> som er <i>god nok</i> til at du kan finne veien videre med andre
     ressurser for å lære mer om det du vil.
     <Db />
-    Uavhengig om du har satt deg ned med denne bloggen med et konkret mål om å lære programmering for framtidig bruk,
-    eller om du bare er nysgjerrig på hva programmering er (og om det er noe for deg?), håper jeg du finner det du leter etter
-    her! 
+    I dét jeg skriver dette, har jeg ennå ikke startet på selve programmeringsinnholdet i bloggen (men jeg kommer til å begynne snart, lover!).
+    Det er altså en sjanse for at bloggen ikke ennå er ferdigskrevet når du starter lesingen.
+    Jeg har per nå ingen fast plan for når nye innlegg skal bli skrevet eller lagt ut, men
+    målet er at nye innlegg i den grunnleggende delen av innføringen skal komme ut med et par dagers, maks én ukes mellomrom.
+    Du kan følge med på <a href="https://github.com/TheVaffel/nocturne.no">Github-siden</a> til denne bloggen
+    for å få en idé om hvilken del av dette nettstedet jeg legger arbeid i til en hver tid.
     <Db />
-    God fornøyelse!
+    Til slutt vil jeg gjøre én ting klart om programmering: Det består av to hovedkomponenter.
+    Det å lære seg grunnkonseptene og "programmeringstenkemåten" er helt nødvendig for å programmere, men for å 
+    kunne jobbe med mer avanserte (og mer spennende?) konsepter som å lage og tegne til vinduer, spille av lyd,
+    lage nettservere og mye annet du kan ha lyst til å lage, må du også kunne bruke andres kode som byggeklosser. Mange 
+    ting (slik som nettopp vinduer) er enormt upraktiske å lage på egen hånd, spesielt i Python, slik at det å benytte seg av byggeklosser,
+    ofte kalt <b>biblioteker</b>, er nødvendig. Det å søke opp og lese dokumentasjon for slike biblioteker er en treningssak, men
+    en introduksjon til hvordan dette kan gjøres vil bli gitt mot slutten av denne innføringen.
+    <Db />
+    Uavhengig om du har satt deg ned med denne bloggen med et konkret mål om å lære programmering for framtidig bruk,
+    eller om du bare er nysgjerrig på hva programmering er (og om det er noe for deg), håper jeg du finner det du leter etter
+    framover i bloggen! 
+    <Db />
+    God fornøyelse og lykke til!
     </>
 );
 
