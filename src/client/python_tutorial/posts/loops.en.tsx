884f1fbd5b7e38983737dfd24d43298a4825c35c
Håkon Flatval <hkon20@hotmail.com>
2021-04-27 20:35:50 +0200

    Add more to English loop post

--- src/client/python_tutorial/posts/loops.en.tsx
+++ src/client/python_tutorial/posts/loops.en.tsx
@@ -93,5 +93,68 @@ Now, i = 4`}
     Having reached the end of the loop block, Python returns to
     the <Ic>while</Ic> line, this time with <Ic>i</Ic> having the
     value 1.
+
+    <Db />
+    Python continues running the loop like this. As we can see in the 
+    output, the <Ic>print()</Ic>-line is run in each iteration of the loop,
+    and the value of <Ic>i</Ic> is incremented by one each time.
+    <Db />
+    In the last iteration of the loop block, <Ic>i</Ic> is equal to 4. The 
+    value is written to screen as before, and <Ic>i</Ic> is incremented
+    to 5. When Python then returns to the <Ic>while</Ic> line,
+    it finds that the condition no longer holds. This is
+    because the inequality 
+    operator <Ic>{`<`}</Ic> evaluates to false if the two values are equal.
+    If we wanted to include an iteration for <Ic>i</Ic> set to 5, we could
+    have used <Ic>{`<=`}</Ic> instead. After having
+    found that the loop condition is false, Python jumps to after the
+    loop block, and reaches the end of the program.
+    <Db />
+    When we reason about loops in programming, it's easy to do a miscalculation
+    in what values will be used in the last iteration of the loop,
+    often called by the common name <b>off-by-one</b>-errors.
+    In the above code, it might be tempting to predict that <Ic>i</Ic> in
+    the last iteration will have the value 5, although, as we discussed
+    above, that is not the case.
+    <Db />
+    A useful way of avoid such errors, is by thinking thoroughly about what 
+    happens in the <i>first</i> and <i>last</i> iterations, and make
+    sure it fits with the intentions you have with the code.
+    <Db />
+    Notice that we only need to change one number in the program to decide
+    how many iterations to run. This is a re-occuring pattern in programming: 
+    If we are able to do something onnce, it is very easy to do it many times.
+    The effort of doing the thing more than once, is handled by the computer,
+    not the programmer. Try to change the 5 in the above program to e.g. 100 
+    and let Python do its job!
+
+    <NoticeBlock>
+        When writing your own <Ic>while</Ic>-loops, it's easy to forget to increment
+        the variable you are using in your loop condition. In such cases, you may
+        end up with Python running the loop again and again forever, since the condition
+        never changes and stays true. If you see that your program uses an
+        unreasonable amount of time, or prints out extremes amounts of output, it's 
+        very likely that this is the cause. Don't panic, it's not dangerous!
+        <Db />
+        To end such an infinite loop, you can interrupt the program. In IDLE, this
+        can be done by clicking <Ic>{`Shell -> Restart Shell`}</Ic>, 
+        or <Ic>Ctrl+F6</Ic> (<Ic>Cmd+F6</Ic> on Mac) on your keyboard. If you
+        are running the program in a terminal, you can usually use <Ic>Ctrl+C</Ic>.
+    </NoticeBlock>
+
+    <h2>We put a block inside a block</h2>
+
+    In the following example, we will use the <Ic>%</Ic>-operator, also called the modulo operator. 
+    It is used with integers, and give the rest after dividing the left hand side by the right.
+    This means that e.g. 5 % 3 gives 2 as an answer, and 7 % 2 gives 1.
+    <Db />
+    If you don't feel a hundred percent sure of how module computations work, 
+    the details aren't that important at the moment. What you need to know, is that 
+    if <i>a</i> % <i>b</i> gives the answer 0, then <i>a</i> is divisible by <i>b</i>, and
+    in particular, this means that if <i>a</i> is an even number, <i>a</i> % 2 will give 0 
+    as the answer.
+
+    
+
     </>
 );
